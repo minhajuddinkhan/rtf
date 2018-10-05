ae352351f429fbeef23a827fb3858ab34bd7af42
EMR <minhajuddin.khan@tenpearls.com>
2018-10-05 17:21:23 +0500

    arg support added

--- commands/convert.go
+++ commands/convert.go
@@ -19,17 +19,27 @@ func Convert(path string) *cli.Command{
 	fileType := "rtf"
 	return &cli.Command{
 		Name: "convert",
-		Aliases: []string{"convert"},
+		Aliases: []string{"convert path/input.html path/output.html"},
 		Usage: "does file conversion",
 		Action: func(c *cli.Context) error {
 			
-			office, err := libreofficekit.NewOffice(path)
-			
-			document, err := office.LoadDocument("abc.html")
+
+			args := c.Args()
+			pathToInput := args.Get(0)
+			pathToOutput := args.Get(1)
+			if len(pathToInput) == 0 {
+				return errors.New("Invalid Args [Input missing] ")
+			} 
+			if len(pathToOutput) == 0 {
+				return errors.New("Invalid Args [Output missing] ")
+			}
+
+			office, err := libreofficekit.NewOffice(path)			
+			document, err := office.LoadDocument(pathToInput)
 			if err != nil {
 				return errors.New(DOC_ERR + err.Error())
 			}
-			err = document.SaveAs("yolo.rtf", fileType, format)
+			err = document.SaveAs(pathToOutput, fileType, format)
 			if err != nil {
 				return errors.New(SAVE_ERR + err.Error())
 			}
